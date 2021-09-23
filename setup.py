d777a43c73349798c66feb3628a2a9226d0bd489
Tim Niven <tim.niven.public@gmail.com>
2021-09-23 13:55:55 +0800

    need a setup file, bump version

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+with open('version') as f:
+    version = f.read().strip()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+setuptools.setup(
+    name='dbi_repositories',
+    version=version,
+    author='Tim Niven',
+    author_email='tim@doublethinklab.org',
+    description='Reusable Repository pattern implementations for multiple DBs.',
+    url=f'https://github.com/timniven/dbi-repositories.git#{version}',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9.5',
+    install_requires=required)
